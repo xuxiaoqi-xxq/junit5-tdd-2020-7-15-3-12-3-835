211e8bd6d07150bb7fcb7d24e8e40a29675e9275
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-23 11:49:52 +0800

    [eva][feature] implement test: given 3 when play fizz buzz whizz then fizz

--- src/main/java/junit5/tdd/FizzBuzzWhizz.java
+++ src/main/java/junit5/tdd/FizzBuzzWhizz.java
@@ -4,6 +4,9 @@ public class FizzBuzzWhizz {
 
 
     public String play(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
