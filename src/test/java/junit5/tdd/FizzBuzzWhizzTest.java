0e8d6dd3b867dda2407654b24221abcc5706288c
xuxiaoqi-xxq <xxq_work@163.com>
2020-07-23 17:56:12 +0800

    [eva]test: impletent should_return_fizzbuzzwhizz_when_play_fizz_buzz_whizz_given_105 test

--- src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzWhizzTest.java
@@ -98,4 +98,17 @@ public class FizzBuzzWhizzTest {
         //then
         assertEquals("BuzzWhizz", actual);
     }
+
+    @Test
+    void should_return_fizzbuzzwhizz_when_play_fizz_buzz_whizz_given_105() {
+        //given
+        int number = 105;
+        FizzBuzzWhizz fizzBuzzWhizz = new FizzBuzzWhizz();
+
+        //when
+        String actual = fizzBuzzWhizz.play(number);
+
+        //then
+        assertEquals("FizzBuzzWhizz", actual);
+    }
 }
